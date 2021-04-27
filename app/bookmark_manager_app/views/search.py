d597f4869aa6af9996e5195c977bd331282045e1
Ram-is-me <ram.s@iiitb.org>
2021-04-27 22:05:25 +0530

    Starting to implement search

---
+++ app/bookmark_manager_app/views/search.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from datetime import date
+from .. import models
+
+def 
+
+def search(request, name, search_content):
