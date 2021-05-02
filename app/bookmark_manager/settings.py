a926d3d7dd1e7110884d4e1a941454cd8b367aa1
Ram-is-me <ram.s@iiitb.org>
2021-05-03 00:31:04 +0530

    Add Reminder Works

--- app/bookmark_manager/settings.py
+++ app/bookmark_manager/settings.py
@@ -199,8 +199,8 @@ LOGOUT_REDIRECT_URL = 'home'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/staticfiles/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
