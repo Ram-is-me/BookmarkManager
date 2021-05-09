097a16cd5db08ccb551dbaed5ea8480adfb8496d
Ram-is-me <ram.s@iiitb.org>
2021-05-09 15:52:55 +0530

    Seeding Data Complete

--- app/bookmark_manager_app/admin.py
+++ app/bookmark_manager_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from bookmark_manager_app.models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Group)
+admin.site.register(Bookmark)
+admin.site.register(Reminder)
+
+
+
+
